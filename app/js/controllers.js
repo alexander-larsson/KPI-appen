b74b1fa82e975defedd05d8d6a88d2a210518bb1
inza <alexx.1992@gmail.com>
2014-08-28 14:05:27 +0200

    Wired up alert message to the controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,4 @@
-/*jslint browser: true*/
+/*jshint browser: true*/
 /*global alert*/
 'use strict';
 
@@ -8,9 +8,10 @@ angular.module('kpi.controllers', [])
 .controller('kpiController', ['$scope', '$http', 'HistoryItem', 'ScbKpi', function($scope, $http, HistoryItem, ScbKpi) {
 
     $scope.history = [];
+    $scope.showInfoState = false;
     //Test below
     $scope.history.push(new HistoryItem(1992,1012,100,150));
-    
+
     ScbKpi.getYears(function(data){
         $scope.years = data.variables[1].values;
         $scope.toYear = $scope.years[$scope.years.length-1];
@@ -18,7 +19,21 @@ angular.module('kpi.controllers', [])
     });
 
     $scope.calculate = function() {
-        //Test below
-        alert(JSON.stringify($scope.history[0]));
+
+        if($scope.toYear === $scope.fromYear) {
+            $scope.showInfoState = true;
+        }else{
+            alert(JSON.stringify($scope.history[0]));
+        }
+
+
+    };
+
+    $scope.showInfo = function() {
+        return $scope.showInfoState;
+    };
+    
+    $scope.hideInfo = function() {
+        $scope.showInfoState = false;
     };
 }]);
